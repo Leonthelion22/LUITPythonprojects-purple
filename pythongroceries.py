06f1bd9178db80538de54b280fe7d59d28849a77
Leonthelion22 <leon.boodram2001@gmail.com>
2024-10-26 13:54:55 -0400

    Update pythongroceries.py

--- pythongroceries.py
+++ pythongroceries.py
@@ -1,7 +1,7 @@
 prices = {'apple': 0.40, 'banana': 0.50, 'kiwi': 0.29,}
 my_purchase = {
     'apple': 13,  #the amount of fruit I bought
-    'banana': 6, # the amout of fruit I bought
+    'banana': 9, # the amout of fruit I bought
     'kiwi':20} # the amount of fruit I bought
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
